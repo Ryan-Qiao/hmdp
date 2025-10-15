7492e20b90a4efdb7c239d8386c644986ce94782
乔国禹 <guoyu.qiao@icloud.com>
2025-10-15 14:36:00 +0800

    sz-04 发送验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -18,11 +21,22 @@ import javax.servlet.http.HttpSession;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     @Override
     public Result sendCode(String phone, HttpSession session) {
+     if(RegexUtils.isPhoneInvalid(phone)){
+         return Result.fail("号码格式有误！");
+     }
 
-        return Result.fail("功能未完成");
+        String numbers = RandomUtil.randomNumbers(6);
+        session.setAttribute("code",numbers);
+
+        log.info("验证码已发送：{}",numbers);
+
+        return Result.ok();
+
+
     }
 }
