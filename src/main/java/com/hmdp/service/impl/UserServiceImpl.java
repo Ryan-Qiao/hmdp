4c0f6ab272e5ba2b7fd8bf853abcc32bfc725cab
乔国禹 <guoyu.qiao@icloud.com>
2025-10-15 15:57:25 +0800

    sz-05

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -38,5 +41,34 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         return Result.ok();
 
 
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("号码格式有误！");
+        }
+
+        Object sessionCode =  session.getAttribute("code");
+        if(sessionCode==null || !loginForm.getCode().equals(sessionCode.toString())){
+            return Result.fail("验证码错误！");
+        }
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        if(user==null){
+            user = createUserWithPhone(phone);
+        }
+
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone){
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+ RandomUtil.randomString(10));
+        //Mybatis-plus
+        save(user);
+        return user;
     }
 }
