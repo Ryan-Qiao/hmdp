0b94081ec3259bf5876f65dddd1181f26ea729f1
乔国禹 <guoyu.qiao@icloud.com>
2025-11-05 14:09:09 +0800

    P93

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -15,13 +15,17 @@ import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -46,16 +50,16 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result sendCode(String phone, HttpSession session) {
-     if(RegexUtils.isPhoneInvalid(phone)){
-         return Result.fail("号码格式有误！");
-     }
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("号码格式有误！");
+        }
 
         String numbers = RandomUtil.randomNumbers(6);
 
         //Use Redis instead of session to store check code
-        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,numbers,LOGIN_CODE_TTL, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, numbers, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
-        log.info("验证码已发送：{}",numbers);
+        log.info("验证码已发送：{}", numbers);
 
         return Result.ok();
 
@@ -66,30 +70,30 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     public Result login(LoginFormDTO loginForm, HttpSession session) {
         //check the format of phone number
         String phone = loginForm.getPhone();
-        if(RegexUtils.isPhoneInvalid(phone)){
+        if (RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("号码格式有误！");
         }
         //get code from Redis
-        Object code = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
+        Object code = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
 
 
-        if(code==null || !loginForm.getCode().equals(code.toString())){
+        if (code == null || !loginForm.getCode().equals(code.toString())) {
             return Result.fail("验证码错误！");
         }
         User user = query().eq("phone", loginForm.getPhone()).one();
-        if(user==null){
+        if (user == null) {
             user = createUserWithPhone(phone);
         }
         //create a token for user
         String token = UUID.randomUUID().toString();
-        UserDTO userDTO = BeanUtil.copyProperties(user,UserDTO.class);
-        Map<String, Object> map = BeanUtil.beanToMap(userDTO,new HashMap<>(), CopyOptions.create()
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> map = BeanUtil.beanToMap(userDTO, new HashMap<>(), CopyOptions.create()
                 .setIgnoreNullValue(true)
-                .setFieldValueEditor((fieldName,fieldValue)->fieldValue.toString()));
+                .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
 //        session.setAttribute("user", userDTO);
-        String tokenKey = LOGIN_USER_KEY+token;
-        stringRedisTemplate.opsForHash().putAll(tokenKey,map);
-        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL,TimeUnit.MINUTES);
+        String tokenKey = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey, map);
+        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
         //save token and user info to the Redis
 
 
@@ -98,15 +102,65 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result logout(String token) {
-        String tokenKey = LOGIN_USER_KEY+token;
+        String tokenKey = LOGIN_USER_KEY + token;
         stringRedisTemplate.delete(tokenKey);
         return Result.ok();
     }
 
-    private User createUserWithPhone(String phone){
+    @Override
+    public Result sign() {
+        // 1.获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2.获取日期
+        LocalDateTime now = LocalDateTime.now();
+        // 3.拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        // 4.获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        // 5.写入Redis SETBIT key offset 1
+        stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+        return Result.ok();
+
+    }
+
+    @Override
+    public Result signCount() {
+        Long userId = UserHolder.getUser().getId();
+        LocalDateTime now = LocalDateTime.now();
+
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        int dayOfMonth = now.getDayOfMonth();
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        if (result == null || result.isEmpty()) {
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        if (num == null || num == 0) {
+            return Result.ok(0);
+        }
+        int count = 0;
+        while (true) {
+            if ((num & 1) == 0) {
+                break;
+            } else {
+                count++;
+            }
+            num >>>= 1;
+        }
+        return Result.ok(count);
+
+    }
+
+    private User createUserWithPhone(String phone) {
         User user = new User();
         user.setPhone(phone);
-        user.setNickName(USER_NICK_NAME_PREFIX+ RandomUtil.randomString(10));
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
         //Mybatis-plus
         save(user);
         return user;
